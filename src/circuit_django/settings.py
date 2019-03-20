b24e46fa41131b79d034744b97e0aacb0d5a6bed
Sun-Mountain <npzonnenberg@gmail.com>
2019-03-20 13:52:30 -0400

    please push

--- src/circuit_django/settings.py
+++ src/circuit_django/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 
     'rest_framework',
     'corsheaders',
