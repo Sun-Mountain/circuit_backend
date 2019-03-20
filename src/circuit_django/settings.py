7c094d584da9cc9789b15a7771c78f1d66240c9a
Sun-Mountain <npzonnenberg@gmail.com>
2019-03-20 13:40:48 -0400

    retry push

--- src/circuit_django/settings.py
+++ src/circuit_django/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
+# STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
